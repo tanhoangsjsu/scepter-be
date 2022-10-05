53b0611d3a3f1b2777f6c79d058d2d9f62a27929
Duc Tan Hoang <ductan.hoang@sjsu.edu>
2022-10-04 20:19:36 -0700

    Insert User Login/SignUp infomation into SQL

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-const express = require("express");
+const express = require("express")
+const mysql = require("mysql")
 const app = express();
+const cors = require("cors")
 
+app.use(express.json())
+app.use(cors())
+
+const db = mysql.createConnection({
+    user:"root",
+    host:"locahost",
+    password:"password",
+    database: "LoginSystem",
+})
+app.post('/register',(req,res)=>{
+    db.query("INSERT INTO users(email,username,password) VALUES(?,?)",
+    [email,username,password], 
+    (err,result)=>{
+        console.log(err)
+    })
+})
 app.listen("8080",()=>{
     console.log("server is running...")
 })
