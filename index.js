9e3c63d61308b293f51d0e5750fb9e3edf0d607c
Duc Tan Hoang <ductan.hoang@sjsu.edu>
2022-10-11 22:05:31 -0700

    Second Edit

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
 const express = require("express")
-const mysql = require("mysql")
+const mongoose = require("mongoose")
 const app = express();
+const bodyParser = require("body-parser")
 const cors = require("cors")
+const dotenv = require("dotenv")
 
 app.use(express.json())
 app.use(cors())
+app.use(bodyParser.json({limit: '30mb'}))
+app.use(bodyParser.urlencoded({extended: true, limit: '30mb'}))
+dotenv.config()
 
-const db = mysql.createConnection({
-    user:"root",
-    host:"locahost",
-    password:"password",
-    database: "LoginSystem",
+mongoose.connect(process.env.MONGODB_URL,()=>{
+    console.log("connected to mongodb")
 })
+
 app.post('/register',(req,res)=>{
-    db.query("INSERT INTO users(email,username,password) VALUES(?,?)",
-    [email,username,password], 
-    (err,result)=>{
-        console.log(err)
-    })
+    const returnedObj = {status:200, message:"successful"}
+    return res.send(returnedObj)
 })
-app.listen("8080",()=>{
+app.post('/login',(req,res)=>{
+    const returnedObj = {status:200, message:"successful", role:"student"}
+    return res.send(returnedObj)
+})
+app.listen(8000,()=>{
     console.log("server is running...")
 })
+
