971f2a3d76021e57c04ef6d8d9728e64840fe8ec
Duc Tan Hoang <ductan.hoang@sjsu.edu>
2022-10-17 20:28:10 -0700

    Fixed bugs

--- models/User.js
+++ models/User.js
@@ -1,42 +1,27 @@
-const mongoose = require('mongoose');
-const { isEmail } = require("validator");
-var uniqueValidator = require("mongoose-unique-validator");
 
-const userSchema = new mongoose.Schema(
-    {
-    username: {
-        type: String,
-        required: [true, "Required"],
-        minlength: [6, "Must be at least 6 characters"],
-        maxlength: [20, "Must be less than 20 characters"],
-        unique: true,
-    },
-    email: {
-        type: String,
-        required: [true, "Required"],
-        maxlength: [50, "Must be 50 characters or less"],
-        unique: true,
-        validate: [isEmail, "Please enter a valid email"],
+const mongoose = require("mongoose")
 
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        require: true,
+        minlength:6,
+        maxlength:20,
     },
-    password: {
-        type: String,
-        required: [true, "Required"],
-        select: false,
-        minlength: [8, "Must be 8 characters or more"],
+    email:{
+        type:String,
+        require: true,
+        minlength:10,
+        maxlength:50,
+    },
+    password:{
+        type:String,
+        require: true,
+        minlength:6,
     },
     role:{
         type:String,
-    },
-    isAssistance: {
-        type: Boolean,
-        default: false,
-    },
-    },
-    { timestamps: true }
-    );
-    userSchema.plugin(uniqueValidator, {
-        message: "Error, expected {PATH} to be unique",
-    });
+    }
+},{timestamps: true})
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", userSchema)
