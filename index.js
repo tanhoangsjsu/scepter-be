2e7d3ae3c13e1c5bab03aabb44e027201d9a6f85
rishabhg-nov <rishabhg.nov@gmail.com>
2022-10-25 17:39:36 -0700

    socket-io back-end

--- index.js
+++ index.js
@@ -5,11 +5,18 @@ const cors = require("cors")
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser")
 const authRoute = require("./routes/auth")
-
-
-
-
 const app = express();
+
+const http = require('http').Server(app);
+// const io = require('socket.io')(http);
+const port = process.env.PORT || 9000;
+const io = require("socket.io")(http, {
+  cors: {
+    origin: '*',
+  }
+});
+
+
 app.use(express.json())
 app.use(cors())
 app.use(cookieParser())
@@ -27,4 +34,21 @@ app.use("/v1/auth", authRoute);
 app.listen(8000,()=>{
     console.log("server is running...")
 })
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+io.on('connection', (socket) => {
+  console.log("Socket.IO got connected!");
+  socket.on("Login", (msg) => {
+    console.log("User logged in!");
+    socket.emit("NewUser", 'userX');
+  });
+});
+
+http.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
+});
 
