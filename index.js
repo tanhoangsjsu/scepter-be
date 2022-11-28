c1aa9c967b450ddac85e66483781aa9b11159e0c
Duc Tan Hoang <ductan.hoang@sjsu.edu>
2022-11-28 07:07:26 -0800

    update socket io

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
         origin: 'http://localhost:3000',
-        methods:["GET","POST"],
+        methods:["GET","POST", "DELETE"],
     }
 });
 
